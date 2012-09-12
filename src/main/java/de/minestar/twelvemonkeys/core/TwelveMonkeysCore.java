e1380b33099318aca1ca8ed1dbd8fad61ccff3d6
Meldanor <Bhaals@gmx.de>
2012-09-12 21:48:47 +0200

    Initial commit
    Signed-off-by: Meldanor <Bhaals@gmx.de>

---
+++ src/main/java/de/minestar/twelvemonkeys/core/TwelveMonkeysCore.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2012 MineStar.de 
+ * 
+ * This file is part of TwelveMonkeys.
+ * 
+ * TwelveMonkeys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ * 
+ * TwelveMonkeys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with TwelveMonkeys.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.minestar.twelvemonkeys.core;
+
+import de.minestar.minestarlibrary.AbstractCore;
+
+public class TwelveMonkeysCore extends AbstractCore {
+
+    public final static String NAME = "TwelveMonkeys";
+
+    public TwelveMonkeysCore() {
+        super(NAME);
+    }
+}
